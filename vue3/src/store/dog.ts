d663da4d718c37df67b722f2966589428f3e27a5
ShunsukeNONOMURA <shunsuke.nonomura@gmail.com>
2023-08-06 19:18:55 +0900

    axios微修正

--- vue3/src/store/dog.ts
+++ vue3/src/store/dog.ts
@@ -5,8 +5,13 @@ export const dogAppStore = () => {
     const dogs: Ref<any[]> = ref([])
 
     async function update(c: number = 10) {
-        const url = `https://dog.ceo/api/breeds/image/random/${c}`
-        const message = await axios.get(url);
+        const config = {
+            url:`https://dog.ceo/api/breeds/image/random/${c}`,  
+            method:'get',
+            // withCredentials: true,
+            // data:formData,
+        };  
+        const message = await axios.request(config);  
         dogs.value = message.data.message
     }
 
